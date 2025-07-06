dae60a33afa16db30d0c89d72c5718951780347a
Luca1234105 <lucadrogo96@gmail.com>
2025-07-06 17:10:13 +0200

    Update Luca.ps1

--- Luca.ps1
+++ Luca.ps1
@@ -47,71 +47,199 @@ function New-SmallButton($text, $location) {
 
 # FUNZIONI PRINCIPALI
 
+function Remove-AppxPackageSafe {
+    param([string]$PackageName)
+    try {
+        Write-Host "Rimuovo $PackageName ..."
+        Get-AppxPackage -AllUsers -Name $PackageName | Remove-AppxPackage -ErrorAction SilentlyContinue
+        Get-AppxProvisionedPackage -Online | Where-Object { $_.PackageName -like "$PackageName*" } | Remove-AppxProvisionedPackage -Online
+        Write-Host "$PackageName rimosso."
+    } catch {
+        Write-Host "Errore nella rimozione di $PackageName"
+    }
+}
+
 function Remove-MicrosoftApps {
-    # Esempio: rimuove app Microsoft preinstallate
-    Get-AppxPackage -AllUsers |
-    Where-Object { $_.Name -notmatch "Store|Calculator" } |
-    Remove-AppxPackage -ErrorAction SilentlyContinue
-    [System.Windows.Forms.MessageBox]::Show("App Microsoft rimosse!")
+    $apps = @(
+        "Clipchamp.Clipchamp",
+        "Microsoft.BingNews",
+        "Microsoft.BingSearch",
+        "Microsoft.BingWeather",
+        "Microsoft.GamingApp",
+        "Microsoft.GetHelp",
+        "Microsoft.MicrosoftOfficeHub",
+        "Microsoft.MicrosoftSolitaireCollection",
+        "Microsoft.MicrosoftStickyNotes",
+        "Microsoft.OutlookForWindows",
+        "Microsoft.Paint",
+        "Microsoft.PowerAutomateDesktop",
+        "Microsoft.ScreenSketch",
+        "Microsoft.Todos",
+        "Microsoft.Windows.DevHome",
+        "Microsoft.WindowsCamera",
+        "Microsoft.WindowsFeedbackHub",
+        "Microsoft.WindowsSoundRecorder",
+        "Microsoft.WindowsTerminal",
+        "Microsoft.Xbox.TCUI",
+        "Microsoft.XboxGamingOverlay",
+        "Microsoft.XboxIdentityProvider",
+        "Microsoft.XboxSpeechToTextOverlay",
+        "Microsoft.YourPhone",
+        "Microsoft.ZuneMusic",
+        "MicrosoftCorporationII.QuickAssist",
+        "MSTeams"
+    )
+    foreach ($app in $apps) {
+        Remove-AppxPackageSafe -PackageName $app
+    }
+    [System.Windows.Forms.MessageBox]::Show("Rimozione app Microsoft completata!","Successo",[System.Windows.Forms.MessageBoxButtons]::OK,[System.Windows.Forms.MessageBoxIcon]::Information)
 }
 
 function Remove-OneDrive {
-    # Rimuove OneDrive completamente
-    Stop-Process -Name OneDrive -ErrorAction SilentlyContinue
-    $paths = @(
-        "$env:SystemRoot\SysWOW64\OneDriveSetup.exe",
-        "$env:SystemRoot\System32\OneDriveSetup.exe"
-    )
-    foreach ($path in $paths) {
-        if (Test-Path $path) { & $path /uninstall }
+    Write-Host "Rimuovo OneDrive..."
+    taskkill /f /im OneDrive.exe > $null 2>&1
+    if (Test-Path "$env:SystemRoot\System32\OneDriveSetup.exe") {
+        & "$env:SystemRoot\System32\OneDriveSetup.exe" /uninstall
     }
-    # Rimuovi residui (esempio)
+    if (Test-Path "$env:SystemRoot\SysWOW64\OneDriveSetup.exe") {
+        & "$env:SystemRoot\SysWOW64\OneDriveSetup.exe" /uninstall
+    }
+    robocopy "$env:USERPROFILE\OneDrive" "$env:USERPROFILE" /mov /e /xj /ndl /nfl /njh /njs /nc /ns /np | Out-Null
+    reg delete "HKEY_CLASSES_ROOT\WOW6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f | Out-Null
+    reg delete "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f | Out-Null
+    Remove-Item "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk" -Force -ErrorAction SilentlyContinue
+    powershell -Command "Get-ScheduledTask -TaskPath '\' -TaskName 'OneDrive*' -ErrorAction SilentlyContinue | Unregister-ScheduledTask -Confirm:\$false" | Out-Null
     Remove-Item "$env:UserProfile\OneDrive" -Recurse -Force -ErrorAction SilentlyContinue
-    [System.Windows.Forms.MessageBox]::Show("OneDrive rimosso!")
+    Remove-Item "$env:LocalAppData\OneDrive" -Recurse -Force -ErrorAction SilentlyContinue
+    Remove-Item "$env:LocalAppData\Microsoft\OneDrive" -Recurse -Force -ErrorAction SilentlyContinue
+    Remove-Item "$env:ProgramData\Microsoft OneDrive" -Recurse -Force -ErrorAction SilentlyContinue
+    Remove-Item "C:\OneDriveTemp" -Recurse -Force -ErrorAction SilentlyContinue
+    reg delete "HKEY_CURRENT_USER\Software\Microsoft\OneDrive" /f | Out-Null
+    [System.Windows.Forms.MessageBox]::Show("OneDrive rimosso con successo!","Successo",[System.Windows.Forms.MessageBoxButtons]::OK,[System.Windows.Forms.MessageBoxIcon]::Information)
 }
 
 function Disable-Copilot {
-    try {
-        # Disabilita Copilot con regkey esempio (ipotetica)
-        Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsCopilot" -Name "AllowCopilot" -Type DWord -Value 0 -Force
-        [System.Windows.Forms.MessageBox]::Show("Copilot disabilitato!")
-    } catch {
-        [System.Windows.Forms.MessageBox]::Show("Errore disabilitazione Copilot: $_")
-    }
+    Write-Host "Disabilito Copilot..."
+    Get-AppxPackage Microsoft.CoPilot | Remove-AppxPackage -ErrorAction SilentlyContinue
+    reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsCopilot" /v "TurnOffWindowsCopilot" /t REG_DWORD /d 1 /f | Out-Null
+    reg add "HKCU\Software\Policies\Microsoft\Windows\WindowsCopilot" /v "TurnOffWindowsCopilot" /t REG_DWORD /d 1 /f | Out-Null
+    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings" /v "AutoOpenCopilotLargeScreens" /t REG_DWORD /d 0 /f | Out-Null
+    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowCopilotButton" /t REG_DWORD /d 0 /f | Out-Null
+    reg add "HKCU\Software\Microsoft\Windows\Shell\Copilot\BingChat" /v "IsUserEligible" /t REG_DWORD /d 0 /f | Out-Null
+    reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "HubsSidebarEnabled" /t REG_DWORD /d 0 /f | Out-Null
+    [System.Windows.Forms.MessageBox]::Show("Copilot disabilitato!","Successo",[System.Windows.Forms.MessageBoxButtons]::OK,[System.Windows.Forms.MessageBoxIcon]::Information)
 }
 
 function Remove-Widgets {
-    # Rimuove widget di Windows 11 (esempio)
-    Get-AppxPackage -AllUsers -Name "MicrosoftWindows.Client.WebExperience" | Remove-AppxPackage -ErrorAction SilentlyContinue
-    [System.Windows.Forms.MessageBox]::Show("Widgets rimossi!")
+    Write-Host "Rimuovo Widgets..."
+    reg add "HKLM\SOFTWARE\Policies\Microsoft\Dsh" /v "AllowNewsAndInterests" /t REG_DWORD /d 0 /f | Out-Null
+    Get-AppxPackage *WebExperience* | Remove-AppxPackage -ErrorAction SilentlyContinue
+    reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\Deprovisioned\MicrosoftWindows.Client.WebExperience_cw5n1h2txyewy" /f | Out-Null
+    [System.Windows.Forms.MessageBox]::Show("Widgets rimossi!","Successo",[System.Windows.Forms.MessageBoxButtons]::OK,[System.Windows.Forms.MessageBoxIcon]::Information)
 }
 
 function Remove-Edge {
-    # Disinstalla Microsoft Edge (versione Chromium)
-    $edgeSetup = "$env:ProgramFiles (x86)\Microsoft\Edge\Application\setup.exe"
-    if (Test-Path $edgeSetup) {
-        & $edgeSetup --uninstall --system-level --verbose-logging --force-uninstall
-        [System.Windows.Forms.MessageBox]::Show("Edge rimosso!")
-    } else {
-        [System.Windows.Forms.MessageBox]::Show("Setup Edge non trovato.")
-    }
+    Write-Host "Rimuovo Microsoft Edge Chromium..."
+    $script = (New-Object Net.WebClient).DownloadString('https://cdn.jsdelivr.net/gh/he3als/EdgeRemover@main/get.ps1')
+    $sb = [ScriptBlock]::Create($script)
+    & $sb -UninstallEdge
+    [System.Windows.Forms.MessageBox]::Show("Microsoft Edge rimosso!","Successo",[System.Windows.Forms.MessageBoxButtons]::OK,[System.Windows.Forms.MessageBoxIcon]::Information)
 }
 
 function Invoke-DisattivaServizi {
-    # Disattiva servizi superflui (esempio)
-    $servicesToDisable = @(
-        "DiagTrack",
-        "WMPNetworkSvc",
-        "XblGameSave",
-        "WaaSMedicSvc"
-    )
-    foreach ($svc in $servicesToDisable) {
-        try {
-            Set-Service -Name $svc -StartupType Disabled -ErrorAction SilentlyContinue
-            Stop-Service -Name $svc -Force -ErrorAction SilentlyContinue
-        } catch {}
-    }
-    [System.Windows.Forms.MessageBox]::Show("Servizi disattivati!")
+    $batContent = '@echo off
+:: Elevazione automatica se non è già admin
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if ''%errorlevel%'' NEQ ''0'' (
+    echo Richiesta dei privilegi amministrativi...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+cls
+color 1f
+echo Disattivazione di tutti i servizi in corso...
+
+for %%S in (
+    vmicguestinterface
+    vmicvss
+    vmicshutdown
+    vmicheartbeat
+    vmicvmsession
+    vmickvpexchange
+    vmictimesync
+    vmicrdv
+    RasAuto
+    workfolderssvc
+    RasMan
+    DusmSvc
+    UmRdpService
+    LanmanServer
+    TermService
+    SensorDataService
+    RetailDemo
+    ScDeviceEnum
+    RmSvc
+    SensrSvc
+    PhoneSvc
+    SCardSvr
+    TapiSrv
+    WSearch
+    LanmanWorkstation
+    MapsBroker
+    SensorService
+    lfsvc
+    PcaSvc
+    SCPolicySvc
+    seclogon
+    SmsRouter
+    wisvc
+    StiSvc
+    CscService
+    WdiSystemHost
+    HvHost
+    SysMain
+    XblAuthManager
+    XblGameSave
+    XboxNetApiSvc
+    XboxGipSvc
+    SessionEnv
+    WpcMonSvc
+    DiagTrack
+    SEMgrSvc
+    MicrosoftEdgeElevationService
+    edgeupdate
+    edgeupdatem
+    CryptSvc
+    BDESVC
+    WbioSrvc
+    bthserv
+    BTAGService
+    PrintNotify
+    WMPNetworkSvc
+    wercplsupport
+    wcncsvc
+) do (
+    sc config %%S start= disabled
+)
+
+echo.
+echo Tutti i servizi selezionati sono stati disattivati.
+echo Riavvia il sistema per applicare le modifiche.
+pause
+exit'
+
+    $tempBat = [System.IO.Path]::Combine($env:TEMP, "DisattivaServizi.bat")
+    [System.IO.File]::WriteAllText($tempBat, $batContent)
+    Start-Process -FilePath $tempBat -Verb RunAs
 }
 
 function Invoke-WinUtilDarkMode {
